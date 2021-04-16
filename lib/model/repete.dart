623e79be1d17e917a6f516e853083557ccac194a
yaslied <yasminborellamed@gmail.com>
2021-04-15 21:02:43 -0400

    Inicio do banco de dados

---
+++ lib/model/repete.dart
@@ -0,0 +1,14 @@
+class Repete {
+  int numero;
+  Recorrencia recorr;
+  DiaSemana diasem;
+
+}
+
+enum Recorrencia {
+  ano, dia, mes, semana
+}
+
+enum DiaSemana {
+  seg, ter, qua, qui, sex, sab, dom
+}
